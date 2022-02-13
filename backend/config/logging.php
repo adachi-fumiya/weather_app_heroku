9d5422751a28b02f8945214b7b9be4837a7de3d6
adachi <adachi@launchcraft.jp>
2022-02-13 16:14:31 +0900

    errorlogが出ない問題を修正

--- backend/config/logging.php
+++ backend/config/logging.php
@@ -50,7 +50,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['stdout'],
             'ignore_exceptions' => false,
         ],
 
